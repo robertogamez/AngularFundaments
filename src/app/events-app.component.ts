810263757295b8994624b95e322772c4f28b3a5f
robertogamez <roberto.mb.gamez@hotmail.com>
2019-02-07 00:09:33 -0600

    Trabajando con componentes

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'events-app',
   template: `
+    <nav-bar></nav-bar>
     <events-list></events-list>
-  `
+  `,
+  styles: [`
+    .nav.navbar-nav { font-size: 15px; }
+    #searchForm { margin-left: 100px; }
+    @media (max-width: 1200px) { #searchForm { display: none } }
+  `]
 })
 export class EventsAppComponent {
   title = 'app';
--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,20 +9,31 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
       <div>Time: {{ event.time }}</div>
       <div>Price: \${{ event.price }}</div>
       <div>
-        <span>Location: {{ event.location.address }}</span> <span>&nbsp;</span>
-        <span>{{ event.location.city }}, {{ event.location.country }}</span>
+        <span>Location: {{ event.location.address }}</span> 
+        
+        <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
       </div>
       <button class="btn btn-primary" (click)="handleClickMe()">
         Click me!
       </button>
     </div>
-  `
+  `,
+  styles: [
+    `.pad-left { margin-left: 10px; }
+     .well div { color: #bbb }
+    `
+  ]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
   @Output() eventClick = new EventEmitter();
+  someProperty: any = "some value";
 
   handleClickMe() {
     this.eventClick.emit(this.event.name);
+  }
+
+  logFoo() {
+    console.log('foo');
   }
 }
