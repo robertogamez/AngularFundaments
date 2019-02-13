dac3fc7d7ec88b94821a0b0d03c662635ab83cee
robertogamez <vn00m8demail.wal/mart.com>
2019-02-13 11:05:59 -0600

    Trabajando con lazy loading y creando barriles de exportaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
-import { NavBarComponent } from './nav/navbar.component';
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator,
+  EventListResolver
+} from "./events/index";
 
-import { EventService } from './events/shared/event.services';
-import { ToastrService } from './common/toastr.services';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
-import { Error404Component } from './errors/404.component';
-import { EventRouteActivator } from './events/event-details/event-route-activator.service';
-import { EventListResolver } from './events/events-list-resolver.services';
+import { EventsAppComponent } from "./events-app.component";
+import { NavBarComponent } from "./nav/navbar.component";
+
+import { ToastrService } from "./common/toastr.services";
+import { RouterModule } from "@angular/router";
+import { appRoutes } from "./routes";
+import { Error404Component } from "./errors/404.component";
 
 @NgModule({
   declarations: [
@@ -27,23 +30,22 @@ import { EventListResolver } from './events/events-list-resolver.services';
     Error404Component
   ],
   providers: [
-    EventService, 
+    EventService,
     ToastrService,
     EventRouteActivator,
     EventListResolver,
-    { provide: 'canDeactiveCreateEvent', useValue: checkDirtyState }
+    { provide: "canDeactiveCreateEvent", useValue: checkDirtyState }
   ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes)
-  ],
+  imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   bootstrap: [EventsAppComponent]
 })
-export class AppModule { }
+export class AppModule {}
 
-export function checkDirtyState(component: CreateEventComponent){
-  if(component.isDirty){
-    return window.confirm('You have not saved this event, do you really want to cancel?');
+export function checkDirtyState(component: CreateEventComponent) {
+  if (component.isDirty) {
+    return window.confirm(
+      "You have not saved this event, do you really want to cancel?"
+    );
   }
 
   return true;
