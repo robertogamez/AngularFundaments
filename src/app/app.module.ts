c3799cbc42bb161da07267897ff760b18ebeb988
robertogamez <roberto.mb.gamez@hotmail.com>
2019-04-07 13:37:00 -0500

    Trabajando con RxJs y Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from '@angular/common/http';
 
 import {
   EventsListComponent,
@@ -70,7 +71,8 @@ let jQuery = window['$'];
     FormsModule, 
     ReactiveFormsModule, 
     // UserModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   bootstrap: [EventsAppComponent]
 })
